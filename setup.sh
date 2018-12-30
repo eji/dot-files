f5225b0882ee8fc7ffaae495f761346faa6535c9
Koji Machida(Ejiri) <k.ejiri@gmail.com>
2018-12-30 22:55:12 +0900

    add fish config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@
 #   ./setup.sh
 #
 
+set -ex
+
 SCRIPT_RELATIVE_PATH=$(dirname $0)
 pushd $SCRIPT_RELATIVE_PATH 1> /dev/null 2>&1
 SCRIPT_ABSOLUTE_PATH=$(pwd)
@@ -17,7 +19,7 @@ do
   echo "s: ${f}-> $HOME/$NEWDOT"
   rm -f $HOME/$NEWDOT
   ln -sf $f $HOME/$NEWDOT
-done;
+done
 
 # ~/.config
 mkdir -p ~/.config
@@ -25,6 +27,6 @@ for f in $SCRIPT_ABSOLUTE_PATH/_config/*
 do
   CONFIG_DIR=$(basename $f)
   echo "s: ${f}-> $HOME/.config/${CONFIG_DIR}"
-  echo "rm -rf $HOME/.config/$CONFIG_DIR"
-  echo "ln -sf $f $HOME/.config/${CONFIG_DIR} "
-done;
+  #echo "rm -rf $HOME/.config/$CONFIG_DIR"
+  ln -sf $f $HOME/.config/${CONFIG_DIR}
+done
