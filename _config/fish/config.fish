25ce1b036be2dcba3a1b6c1e20fa1c1f435764b2
Koji Machida(Ejiri) <k.ejiri@gmail.com>
2018-12-31 17:36:14 +0900

    fixed bind

--- _config/fish/config.fish
+++ _config/fish/config.fish
@@ -3,3 +3,8 @@ if status is-interactive
   . (rbenv init - | psub)
 end
 set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
+
+bind \ce '__ghq_crtl_g'
+if bind -M insert >/dev/null ^/dev/null
+    bind -M insert \ce '__ghq_crtl_g'
+end
